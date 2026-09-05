310dd06afa62f92cd30fd3858609360a820b0ce3
8fgb66gmt2-stack <8fgb66gmt2@privaterelay.appleid.com>
2026-09-05 13:34:01 +0900

    帝國通信のサービスワーカーを追加

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='teikoku-tsushin-v1';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const copy=x.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return x}).catch(()=>caches.match('./index.html'))))});
+self.addEventListener('notificationclick',e=>{e.notification.close();e.waitUntil(clients.matchAll({type:'window'}).then(cs=>cs.length?cs[0].focus():clients.openWindow('./')))});
